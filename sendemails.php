f2fb953b99a23e20f069cafc2b4009af508f3e44
Artizteca <artizteca@gmail.com>
2024-06-03 20:45:35 -0600

    Tercer commit

---
+++ sendemails.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['Email-2'];
+
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $to = "artizteca@gmail.com";
+        $subject = "Nuevo contacto desde el formulario";
+        $message = "Has recibido un nuevo mensaje desde tu formulario de contacto.\n\nCorreo: $email";
+        $headers = "From: no-reply@artiztea.com\r\n";
+        $headers .= "Reply-To: $email\r\n";
+        
+        if (mail($to, $subject, $message, $headers)) {
+            echo 'success';
+        } else {
+            echo 'error';
+        }
+    } else {
+        echo 'error';
+    }
+} else {
+    echo 'error';
+}
+?>
